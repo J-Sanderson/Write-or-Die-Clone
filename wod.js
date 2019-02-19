0f215b58632b6586b22430cd39eb7e89a044889c
J Sanderson <ju.a.sanderson@gmail.com>
2019-02-19 13:36:58 +0000

    improve timeup functionality

--- wod.js
+++ wod.js
@@ -1,28 +1,31 @@
-var keyupTimer = 0;
-var mode;
-var grace;
-var sounds = {
-  baby: new Audio("http://soundbible.com/grab.php?id=557&type=mp3"),
-  speaker: new Audio("http://soundbible.com/grab.php?id=2085&type=mp3"),
-  buzzer: new Audio("http://soundbible.com/grab.php?id=1811&type=mp3"),
-  mosquito: new Audio("http://soundbible.com/grab.php?id=1591&type=mp3"),
-  alarm: new Audio("http://soundbible.com/grab.php?id=1787&type=mp3"),
-  bagpipes: new Audio("http://soundbible.com/grab.php?id=527&type=mp3"),
-}
-var sound;
-var keyupCheck;
-var deleteWords;
+var pauseUsed,
+  settings,
+  mode,
+  time,
+  grace,
+  running,
+  secondCheck,
+  keyupCheck,
+  keyupTimer = 0,
+  sound,
+  deleteWords,
+  sounds = {
+    baby: new Audio("http://soundbible.com/grab.php?id=557&type=mp3"),
+    speaker: new Audio("http://soundbible.com/grab.php?id=2085&type=mp3"),
+    buzzer: new Audio("http://soundbible.com/grab.php?id=1811&type=mp3"),
+    mosquito: new Audio("http://soundbible.com/grab.php?id=1591&type=mp3"),
+    alarm: new Audio("http://soundbible.com/grab.php?id=1787&type=mp3"),
+    bagpipes: new Audio("http://soundbible.com/grab.php?id=527&type=mp3")
+  };
 
 document.addEventListener("DOMContentLoaded", function() {
-  
   document.getElementById("settings").addEventListener("submit", function(e) {
-
     e.preventDefault();
 
-    //http://javascript-coder.com/javascript-form/javascript-get-form.phtml
-    var settings = $("#settings").serializeArray();
+    pauseUsed = false;
+    settings = $("#settings").serializeArray();
     mode = settings[0].value;
-    var time = settings[1].value * 60; //in seconds
+    time = settings[1].value * 60; //in seconds
     grace = parseInt(settings[2].value);
     $("#time-remaining").append(convertMinutes(time));
 
@@ -32,10 +35,10 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("credit-container").classList.add("hidden");
 
     //set running status
-    var running = true;
+    running = true;
 
     //perform a check every second
-    var secondCheck = setInterval(tick, 1000);
+    secondCheck = setInterval(tick, 1000);
     //start time since typed timer
     keyupCheck = setInterval(timeSinceKeyup, 1000);
 
@@ -46,17 +49,18 @@ document.addEventListener("DOMContentLoaded", function() {
         time -= 1;
         //is the time up?
         if (time < 0) {
-          document.getElementById("time-remaining")
-            .innerHTML = "00:00";
-          alert("Time's up!");
+          document.getElementById("time-remaining").innerHTML = "00:00";
+          document.getElementById("timeup-screen").classList.remove("hidden");
+          document.getElementById("overlay").classList.remove("hidden");
           //running status off
           running = false;
           clearInterval(secondCheck);
           clearInterval(keyupCheck);
         } else {
           //display new time
-          document.getElementById("time-remaining")
-            .innerHTML = convertMinutes(time);
+          document.getElementById("time-remaining").innerHTML = convertMinutes(
+            time
+          );
         }
       }
     }
@@ -67,11 +71,13 @@ document.addEventListener("DOMContentLoaded", function() {
     var wc = getWordCount(document.getElementById("livetext").value);
     document.getElementById("wordcount").innerHTML = wc;
     //reset keyup timer
-    keyupTimer = 0;
-    clearInterval(keyupCheck);
-    keyupCheck = setInterval(timeSinceKeyup, 1000);
-    document.getElementById("livetext").removeAttribute("class");
-    document.getElementById("livetext").classList.add("warning-none");
+    if (running) {
+      keyupTimer = 0;
+      clearInterval(keyupCheck);
+      keyupCheck = setInterval(timeSinceKeyup, 1000);
+      document.getElementById("livetext").removeAttribute("class");
+      document.getElementById("livetext").classList.add("warning-none");
+    }
     //reset sound loop if applicable
     if (sound) {
       sound.loop = false;
@@ -79,7 +85,8 @@ document.addEventListener("DOMContentLoaded", function() {
       sound.currentTime = 0;
     }
     //reset kamikaze mode if applicable
-    if (deleteWords); {
+    if (deleteWords);
+    {
       clearInterval(deleteWords);
     }
   });
@@ -87,11 +94,11 @@ document.addEventListener("DOMContentLoaded", function() {
   function timeSinceKeyup() {
     //increase the timer
     keyupTimer++;
-    //75% or 50% of the wqy to grace?
-    if (keyupTimer/grace > 0.75 && keyupTimer/grace < 1) {
+    //75% or 50% of the way to grace?
+    if (keyupTimer / grace > 0.75 && keyupTimer / grace < 1) {
       document.getElementById("livetext").removeAttribute("class");
       document.getElementById("livetext").classList.add("warning-second");
-    } else if (keyupTimer/grace > 0.5 && keyupTimer/grace < 1) {
+    } else if (keyupTimer / grace > 0.5 && keyupTimer / grace < 1) {
       document.getElementById("livetext").removeAttribute("class");
       document.getElementById("livetext").classList.add("warning-first");
     }
@@ -112,7 +119,7 @@ document.addEventListener("DOMContentLoaded", function() {
       } else if (mode === "intermediate") {
         //select sound to play (should be random)
         var keys = Object.keys(sounds);
-        sound = sounds[keys[Math.floor((Math.random() * keys.length))]];
+        sound = sounds[keys[Math.floor(Math.random() * keys.length)]];
         sound.loop = true;
         sound.play();
       } else {
@@ -121,21 +128,45 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
   }
-  
+
   function kamikaze() {
     var words = document.getElementById("livetext").value.split(" ");
     words.pop();
     document.getElementById("livetext").value = words.join(" ");
   }
-  
+
   //credits show/hide
-  document.getElementById("credit-button").addEventListener("click", function() {
-    document.getElementById("credits").classList.remove("hidden");
-  });
-  document.getElementById("close-credits").addEventListener("click", function() {
-    document.getElementById("credits").classList.add("hidden");
-  });
+  document
+    .getElementById("credit-button")
+    .addEventListener("click", function() {
+      document.getElementById("credits").classList.remove("hidden");
+      document.getElementById("overlay").classList.remove("hidden");
+    });
+  document
+    .getElementById("close-credits")
+    .addEventListener("click", function() {
+      document.getElementById("credits").classList.add("hidden");
+      document.getElementById("overlay").classList.add("hidden");
+    });
   
+  //dismiss timeup box
+  document
+    .getElementById("written")
+    .addEventListener("click", function() {
+      document.getElementById("timeup-screen").classList.add("hidden");
+      document.getElementById("overlay").classList.add("hidden");
+    });
+
+  //pause
+  document.getElementById("pause").addEventListener("click", function() {
+    if (!pauseUsed) {
+      alert(
+        "You are now paused! Click OK to return to your writing. You will NOT be able to pause again for this session!"
+      );
+      pauseUsed = true;
+      document.getElementById("pause").classList.add("inactive");
+    }
+  });
 });
 
 //converts second count to a mm:ss string for display
